0dcb2d8355be9437d67dfe38809a28ba067b689c
Leo Ris <ristanovski@hotmail.com>
2021-12-26 12:05:16 +0100

    Unit testing on DemeritPointsCalculator class.

---
+++ Vehicle.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Vehicle.UnitTests
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTests
+    {
+        [Test]
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void CalculateDemeritPoints_WhenCalled_ThrowOutOfRangeException(int speed)
+        {
+            var calculator = new DemeritPointsCalculator();
+
+            Assert.That(() => calculator.CalculateDemeritPoints(speed), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(64, 0)]
+        [TestCase(65, 0)]
+        [TestCase(66, 0)]
+        [TestCase(70, 1)]
+        [TestCase(75, 2)]
+        public void CalculateDemeritPoints_SpeedHigherThanLimitFor5km_Get1DemeritPoint(int speed, int expectedResult)
+        {
+            var speedTest = new DemeritPointsCalculator();
+
+            var result = speedTest.CalculateDemeritPoints(speed);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
